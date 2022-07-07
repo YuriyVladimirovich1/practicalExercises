f08458aa74c9fda34d8996568bb30f2c3c4a223f
Yuriy <a.yuriy.v@mail.ru>
2022-07-07 14:39:44 +0300

    sem10

---
+++ Sem10/Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Задача 70: Напишите программу, которая на вход принимает два числа и выдаёт 
+первые N чисел, для которых каждое следующее равно сумме двух предыдущих. 
+3 и 4, N = 5 -> 3 4 7 11 18 
+6 и 10, N = 4 -> 6 10 16 26  
+ */ 
+ 
+Console.Clear(); 
+int SumPreviosTwo(int numberStart, int numberSecond, int number) 
+{ 
+    if (number == 1) return numberStart; 
+ 
+    if (number == 2) return numberSecond; 
+ 
+    return SumPreviosTwo(numberStart, numberSecond, number - 1) + SumPreviosTwo(numberStart, numberSecond, number - 2); 
+} 
+ 
+ 
+Console.WriteLine("Введите numberStart"); 
+int numberStart = int.Parse(Console.ReadLine()); 
+Console.WriteLine("Введите numberFinish"); 
+int numberSecond = int.Parse(Console.ReadLine()); 
+Console.WriteLine("Введите N"); 
+int number = int.Parse(Console.ReadLine()); 
+Console.WriteLine(); 
+ 
+for (int i = 1; i <= number; i++) 
+{ 
+Console.Write(SumPreviosTwo(numberStart, numberSecond, i) + " ");     
+}
---
+++ Sem10/Task3/Program.cs
@@ -0,0 +1,39 @@
+﻿//: Заданы 2 массива: info и data. В массиве info хранятся двоичные представления нескольких чисел (без разделителя). 
+// В массиве data хранится информация о количестве бит, которые занимают числа из массива info. 
+// Напишите программу, которая составит массив десятичных представлений чисел массива data с учётом информации из массива info. 
+ 
+void PrintArray(int[] arr) 
+{  
+     
+        for (int i = 0; i < arr.Length; i++) 
+        { 
+            Console.Write(arr[i] + " "); 
+        } 
+        Console.WriteLine(); 
+     
+} 
+ 
+ 
+int [] data = {0,0,1,1,0,0,1,1,0}; 
+int [] info = {2,3,3,1}; 
+ 
+int [] cdata = new int [info.Length]; 
+ 
+int k=0; 
+ 
+for (int i = 0; i < info.Length; i++) 
+{ 
+    for (int j = info[i]-1, m=0; j >= 0; j--,m++) 
+    { 
+         
+        Console.Write($"{data[m+k]}"); 
+ 
+         
+        cdata[i]+=(int)(data[j+k]*Math.Pow(2,m)); 
+         
+    } 
+         
+    k+=info[i]; 
+    Console.WriteLine(); 
+} 
+PrintArray(cdata);
